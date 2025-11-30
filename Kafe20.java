3908f187b9fab01a857a272c6eac1e4b6779c9d7
Naufal-farhan <nefener123@gmail.com>
2025-11-30 13:01:24 +0700

    Percobaan-1

---
+++ Kafe20.java
@@ -0,0 +1,17 @@
+public class Kafe20{
+    public static void main(String[] args) {
+        menu();
+    }
+    public static void menu(){
+        System.out.println("==== MENU RESTO KAFE ====");
+        System.out.println("1. Kopi Hitam - RP 15.000");
+        System.out.println("2. Capucinno  - RP 13.000");
+        System.out.println("3. Latte      - RP 72.000");
+        System.out.println("4. Teh Tarik  - RP 25.000");
+        System.out.println("5. Roti Bakar - RP 65.000");
+        System.out.println("6. Mie Goreng - RP 13.000");
+        System.out.println("7. Kopi Hitam - RP 16.000");
+        System.out.println("===========================");
+        System.out.println("Silahkan Pilih Menu!!!!...");
+    }   
+}

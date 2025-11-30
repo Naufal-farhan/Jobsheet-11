0b53c6067d195edbd711071d7e94e3c4ffed3ed4
Naufal-farhan <nefener123@gmail.com>
2025-11-30 14:00:23 +0700

    Modifikasi percobaan-2

--- Kafe20.java
+++ Kafe20.java
@@ -1,11 +1,25 @@
+import java.util.*;
 public class Kafe20{
     public static void main(String[] args) {
-        menu("Andi",true);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan kode promo! : ");
+        String diskon = sc.nextLine();
+        menu("Andi",true, diskon);
     }
-    public static void menu(String namaPelanggan, boolean isMember){
+    public static void menu(String namaPelanggan, boolean isMember, String kodePromo){
         System.out.println("Selaamt Datang, "+namaPelanggan+" !");
         if(isMember){
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!!..");
+        }
+        if(kodePromo.equals("DISKON50")){
+            System.out.println("Anda Mendapatkan diskon tambahan sebesar 50% !!..");
+        }
+        else if(kodePromo.equals("DISKON30")){
+            System.out.println("Anda Mendapatkan diskon tambahan sebesar 30% !!..");
+        }
+        else{
+            System.out.println("Kode Promo anda tidak valid !!..");
+
         }
 
         System.out.println("==== MENU RESTO KAFE ====");
